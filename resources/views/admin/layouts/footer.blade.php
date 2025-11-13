ff4e304534d7a96ddcee5b38952acabb58a47bd9
Devildaus <devildaus666@gmail.com>
2025-11-13 08:04:01 +0700

    master

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+     All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- jquery-validation -->
+<script src="/plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="/plugins/jquery-validation/additional-methods.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/dist/js/adminlte.min.js?v=3.2.0"></script>
+<!-- AdminLTE for demo purposes -->
+
+</body>
+</html>
---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <a href="" class="btn btn-primary mb-2"><i class="fas fa-plus"></i> Tambah</a>
+
+
+                <table class="table">
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>budi</td>
+                        <td>budi@gmail</td>
+                        <td>Admin</td>
+                        <td>
+                            <div class="d-flex">
+                                <a href="" class="btn btn-info mr-2"><i class="fas fa-edit"></i> Edit</a>
+                                <div class="form">
+                                    <button class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
